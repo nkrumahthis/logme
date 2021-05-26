31516061c176044b85387bbbe18a41870d35ffbe
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-05-26 18:43:26 +0000

    first commit

---
+++ Driver.java
@@ -0,0 +1,26 @@
+/**
+ * Main
+ */
+public class Driver {
+
+    private static int add(int a, int b){
+        return(a + b);
+    }
+
+    private static int subtract(int a, int b){
+        return (a - b);
+    }
+
+    private static int multiply(int a, int b){
+        return (a * b);
+    }
+
+    private static double divide(int a, int b){
+        return (a / b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    
+}
